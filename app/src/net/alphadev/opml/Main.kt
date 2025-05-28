944df4735a2b64c83d1513dc344d89c9ab3a5038
Jan Seeger <jan@alphadev.net>
2025-05-29 00:48:31 +0200

    Fix Main package

--- app/src/net/alphadev/opml/Main.kt
+++ app/src/net/alphadev/opml/Main.kt
@@ -1,12 +1,10 @@
+package net.alphadev.opml
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.main
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.types.file
 import kotlinx.io.files.Path
-import net.alphadev.opml.formatFile
-import net.alphadev.opml.readFile
-import net.alphadev.opml.writeFile
 import kotlin.time.measureTime
 
 fun main(args: Array<String>) = OpmlTool().main(args)
